433d331d8ec03bf4cde262c2072157d50b5fc158
Erik Froese <erik@hhd.com.au>
2015-06-29 18:00:09 +1000

    using REXML to update default.xml; add mappings of possible elements

--- lib/cloudhub_wrapper.rb
+++ lib/cloudhub_wrapper.rb
@@ -3,6 +3,8 @@ class CloudhubWrapper
   require "net/https"
   require "uri"
   require "builder"
+  require "rexml/document"
+  
 
   def initialize(endpoint, user, password)
     @endpoint = endpoint
@@ -19,7 +21,32 @@ class CloudhubWrapper
   end
 
   def build_xml(subscriber, channel, origin, schema)
-    #@xml = 
+    address_elements = [
+      [:address, "addressLine"],
+      [:suburb, "suburb"],
+      [:postcode, "postalcode"],
+      [:post_code, "postalcode"],
+      [:state, "state"]
+    ]
+    
+    contact_elements = [
+      [:first_name, "firstName"],
+      [:last_name, "lastName"],
+      [:email, "email"],
+      [:mobile, "phone"],
+      [:phone, "phone"]
+    ]
+
+
+    doc = REXML::Document.new(File.read("data/default.xml"))
+    root = doc.root
+    root.add_attributes({"interchangeDate" => Time.now.to_s, "xmlns:ns0" => schema})
+    root.elements["ns0:account"].add_attributes({"id" => subscriber.email, "creationDateTime" => Time.now.to_s, "xmlns:ns0" => schema})
+    root.elements[1].elements["ns0:address"]
+
+
+    @xml = doc.to_s #turn this into xml 
+    
   end
 
   def post_xml
