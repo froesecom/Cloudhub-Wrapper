13a50797c718528a8f8f552e343f9a3fc68ee62a
Erik Froese <erik@hhd.com.au>
2015-06-29 16:25:33 +1000

    move default xml into seperate file

--- lib/cloudhub_wrapper.rb
+++ lib/cloudhub_wrapper.rb
@@ -2,6 +2,7 @@ class CloudhubWrapper
 
   require "net/https"
   require "uri"
+  require "builder"
 
   def initialize(endpoint, user, password)
     @endpoint = endpoint
@@ -18,27 +19,7 @@ class CloudhubWrapper
   end
 
   def build_xml(subscriber, channel, origin, schema)
-    @xml = "<ns0:accountInterchange interchangeControlNo='00000001' agreementName='SF.LE' senderQualifier='ZZ' senderId='LE' receiverQualifier='ZZ' receiverId='SF' interchangeDate='#{ Time.now.to_s }' xmlns:ns0='#{schema}'>
-              <ns0:account id='#{ subscriber.email }' domain='external' type='Retail Account'  creationDateTime='#{ Time.now.to_s }' xmlns:ns0='#{schema}'>
-                <ns0:address addressType='home'>
-                  <ns0:addressLine>#{ subscriber.address }</ns0:addressLine>
-                  <ns0:suburb>#{ subscriber.suburb }</ns0:suburb>
-                  <ns0:postalcode>#{ subscriber.postcode }</ns0:postalcode>
-                  <ns0:state>#{ subscriber.state }</ns0:state>
-                  <ns0:country isoCountryCode='AU'>Australia</ns0:country>
-                </ns0:address>
-                <ns0:contact contactRole='requestor'>
-                  <ns0:firstName>#{ subscriber.first_name }</ns0:firstName>
-                  <ns0:lastName>#{ subscriber.last_name }</ns0:lastName'
-                  <ns0:email>#{ subscriber.email }</ns0:email>
-                  <ns0:phone>#{ subscriber.mobile }</ns0:phone>
-                  <ns0:contactPreference contactVia='Email' contactOption='OptIn'/>
-                <ns0:channelContactPreferences>
-                  <ns0:channelContactPreference channel='#{ channel }' contactOption='optIn' origin='#{ origin }'/>
-                </ns0:channelContactPreferences>
-              </ns0:contact>
-              </ns0:account>
-            </ns0:accountInterchange>"
+    #@xml = 
   end
 
   def post_xml
